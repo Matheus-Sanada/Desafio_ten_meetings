4d54fb8a5b8f1ed66ac000da6fc5d5bcd94f67ad
Matheus-Sanada <matheus.dev@outlook.com.br>
2022-06-02 21:58:28 -0300

    Primeiro commit

---
+++ javascript/global.js
@@ -0,0 +1,23 @@
+// STATUS DA ASSEMBLEIA
+
+
+function assembly() {
+
+    alert("Esta assembleia esta ativada.")
+    
+}
+
+function noassembly() {
+
+    alert("Esta assembleia esta inativa, favor voltar outro dia :).")
+    
+}
+
+
+// ENTRADA PAUTAS
+
+function activate() {
+
+    alert("Seja-bem vindo(a), fique a vontade para tomar a sua decisão :)")
+    
+}
